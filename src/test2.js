cf043c1342d036cfea3646d59d41cde58aca73c3
Zenquan <1847867150@qq.com>
2022-04-19 13:33:49 +0800

    A: 补充生成d.ts的案例

--- src/test2.js
+++ src/test2.js
@@ -1,3 +1,7 @@
+/**
+ * 测试使用
+ * @param  {...any} arg 参数
+ */
 export function test2(...arg) {
-  console.log('test2>>>', arg);
+  console.log("test2>>>", arg);
 }
---
+++ types/types.d.ts
@@ -0,0 +1,6 @@
+/**
+ * 测试使用
+ * @param arg - 参数
+ */
+declare function test2(...arg: any[]): void;
+
