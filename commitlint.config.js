533b07fd7793d5f401ba248c971e75111379cb22
Zenquan <1847867150@qq.com>
2022-04-23 22:21:55 +0800

    fix: 修改commit提交规范

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,16 +1,31 @@
 module.exports = {
   rules: {
-    "subject-case": [0, "never"],
-    "type-empty": [2, 'never'],
-    "type-enum": [
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [1, 'always'],
+    'header-max-length': [2, 'always', 108],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+    'subject-case': [0],
+    'type-enum': [
       2,
-      "always",
+      'always',
       [
-        "A", // "添加一个新的功能模块"
-        "M", // "更新一个功能模块",
-        "B", // "构建静态资源",
-        "F", // "修复 X 页面的问题"
-      ]
-    ]
-  }
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'workflow',
+        'types',
+        'release',
+      ],
+    ],
+  },
 };
