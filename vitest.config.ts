30b5b78494ed750ce8272b7731afde2fe36f8b07
Zenquan <1847867150@qq.com>
2022-05-02 19:54:35 +0800

    fix: 解决测试中canvas、window、document is not defined问题

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { UserConfig } from 'vitest';
 
 const config: { test: UserConfig } = {
   test: {
+    environment: 'jsdom',
+    threads: false,
+    environmentOptions: {
+      jsdom: {
+        resources: 'usable',
+      },
+    },
     testTimeout: 50000,
   },
 };
