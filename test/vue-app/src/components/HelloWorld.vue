0008d479beae3af3c72a0b21dc6e00664bc3c390
Zenquan <1847867150@qq.com>
2023-01-04 15:43:15 +0800

    feat: 新增test目录vue-app

---
+++ test/vue-app/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+import { Button } from '../../../../dist/index.esm.js';
+import '../../../../dist/js-lib-starter.css';
+
+const count = ref(0);
+</script>
+
+<template>
+  <Button />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
