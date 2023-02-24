bf9f6a10d97090bd3a58b6ceeca501a70e37ef46
Zenquan <1847867150@qq.com>
2023-02-24 21:56:52 +0800

    chore: add some new config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,24 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    'vue/setup-compiler-macros': true,
   },
-  extends: ['eslint-config-prettier', 'eslint:recommended'],
-  plugins: ['eslint-plugin-prettier'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
   },
   rules: {
     'no-console': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'vue/multi-word-component-names': 'warn',
   },
 };
